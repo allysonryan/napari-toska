8ae9c376d13bc39258385638e78ce59c9e7abfc0
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-06-29 15:06:57 +0200

    added function to calculate all adjacency matrices

--- src/napari_toska/_network_analysis.py
+++ src/napari_toska/_network_analysis.py
@@ -1,5 +1,45 @@
 import numpy as np
 
+
+def calculate_all_adjancency_matrices(
+        labelled_skeletons: "napari.types.LabelsData",
+        parsed_skeletons: "napari.types.LabelsData",
+        neighborhood: str
+):
+    """
+    Calculate all adjacency matrices for a given skeleton image.
+
+    Parameters:
+    -----------
+    labelled_skeletons: napari.types.LabelsData
+        A skeleton image with each skeleton carrying a unique label.
+    parsed_skeletons: napari.types.LabelsData
+        A skeleton image where each pixel is labelled according to the
+        point type which can be either a terminal point (1), a branching
+        point (3), or a chain point (2).
+    neighborhood: str
+        The neighborhood connectivity of the skeleton. Can be "n4",
+        "n6", "n8", "n18", or "n26".
+
+    Returns:
+    --------
+    adjacency_matrices: list
+        A list of adjacency matrices for each skeleton in the image.
+    """
+    skeleton_ids = np.unique(labelled_skeletons)[1:]
+    adjacency_matrices = [0] * len(skeleton_ids)
+
+    skeleton_counter = 0
+    for skeleton_id in skeleton_ids:
+        # create a sub-skeleton image with only one skeleton with values
+        # 0 (background) and 1 (skeleton end point) etc.
+        sub_skeleton = parsed_skeletons * (labelled_skeletons == skeleton_id)
+        adjacency_matrices[skeleton_counter] = create_adjacency_matrix(sub_skeleton, neighborhood)
+        skeleton_counter += 1
+
+    return adjacency_matrices
+
+
 def create_adjacency_matrix(labelled_skeletons: "napari.types.LabelsData",
                             neighborhood: str = "n4") -> np.ndarray:
     from scipy import ndimage
