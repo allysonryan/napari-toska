0722c0817886e82a5e2cc5e44d68b7c15eec56c3
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-07-08 13:59:00 +0200

    fixed version number for future releases

--- src/napari_toska/__init__.py
+++ src/napari_toska/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 from ._labeled_skeletonization import (generate_labeled_skeletonization,
                                        parse_single_skeleton,
                                        parse_all_skeletons,
