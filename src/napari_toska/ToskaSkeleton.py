db9ba9187b79b2547edebf31e28de77d3d96337b
Johannes Müller <johannes.mueller88@hotmail.de>
2025-01-26 14:37:59 +0100

    fixed unique labels map

--- src/napari_toska/ToskaSkeleton.py
+++ src/napari_toska/ToskaSkeleton.py
@@ -6,49 +6,61 @@ import pandas as pd
 
 
 class ToskaSkeleton(Labels):
-    def __init__(self, skeleton_data, neighborhood, **kwargs):
-        super().__init__(skeleton_data, **kwargs)
+    def __init__(self, labels_data: "napari.types.LabelsData", neighborhood, **kwargs):
+        super().__init__(labels_data, **kwargs)
 
         self._neighborhood = neighborhood
-        self._graph = nx.Graph()
+        self.graph = nx.Graph()
         
-        self._parse_skeleton(skeleton_data)
+        self._parse_skeleton(labels_data)
+        self._build_nx_graph()
 
     @property
     def neighborhood(self):
         return self._neighborhood
     
-    def _parse_skeleton(self, skeleton):
+    @property
+    def graph(self) -> nx.Graph:
+        return self.metadata.get('graph')
+    
+    @graph.setter
+    def graph(self, graph: nx.Graph):
+        self.metadata['graph'] = graph
+    
+    def _parse_skeleton(self, label_image: "napari.types.LabelsData") -> None:
         from skimage import measure
 
-        labelled_skeletons = generate_labeled_skeletonization(skeleton)
+        labelled_skeletons = generate_labeled_skeletonization(label_image)
         parsed_skeletons = parse_all_skeletons(labelled_skeletons, neighborhood=self._neighborhood)
         end_points = measure.label(parsed_skeletons == 1)
         branches = measure.label(parsed_skeletons == 2)
         branch_points = measure.label(parsed_skeletons == 3)
 
-        branches[branches > 0] += end_points.max()
-        branch_points[branch_points > 0] += end_points.max() + branches.max()
+        # create a labels image with unique labels for each object
+        n_endpoints = end_points.max()
+        n_branches = branches.max()
+        branches[branches != 0] += n_endpoints
+        branch_points[branch_points != 0] += n_endpoints + n_branches
 
         unique_labels = end_points + branches + branch_points
-        self.data = unique_labels
 
         # add object types to features (branch/end/chain)
         object_types = np.ones((unique_labels.max()), dtype=int)
-        object_types[:end_points.max()] = 1
-        object_types[end_points.max() + 1:branches.max() + end_points.max() + 1] = 2
-        object_types[-branches.max():] = 3
+        object_types[:n_endpoints] = 1
+        object_types[n_endpoints:n_endpoints + n_branches] = 2
+        object_types[n_endpoints + n_branches:] = 3
 
-        # infer skeleton id from labelled skeleton
-
+        self.data = unique_labels
         self.features = pd.DataFrame({
-            'labels': np.arange(1, self.data.max() + 1),
-            'object_type': object_types
+            'label': np.arange(1, unique_labels.max() + 1),
+            'object_type': object_types,
         })
 
+        # add skeleton ID to features
+        for i, row in self.features.iterrows():
+            self.features.loc[i, 'skeleton_id'] = labelled_skeletons[unique_labels == row['label']][0]
 
-        
-        
+        return
     
     def _build_nx_graph(self):
         from skimage import morphology
