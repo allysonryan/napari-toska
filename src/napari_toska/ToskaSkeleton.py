1fff9e26afafd2db7d456ef00e825ca3541eda4f
Johannes Müller <johannes.mueller88@hotmail.de>
2025-01-26 14:37:46 +0100

    added graph building

--- src/napari_toska/ToskaSkeleton.py
+++ src/napari_toska/ToskaSkeleton.py
@@ -50,4 +50,45 @@ class ToskaSkeleton(Labels):
         
         
     
-#register_layer_type(ToskaSkeleton, 'toska_skeleton')
+    def _build_nx_graph(self):
+        from skimage import morphology
+        import tqdm
+
+        # add all branch points and end points to Graph
+        for _, row in self.features[self.features['object_type'] != 2].iterrows():
+            self.graph.add_node(row['label'],
+                                    object_type=row['object_type'],
+                                    label=row['label'])
+        
+
+        df_branches = self.features[self.features['object_type'] == 2]
+
+        for i, row in tqdm.tqdm(df_branches.iterrows(), desc='Building Graph', total=len(df_branches)):
+            branch = self.data == row['label']
+            branch_point_coordinates = np.asarray(np.where(branch)).T
+
+            # get bounding box around branch
+            min_coords = branch_point_coordinates.min(axis=0) - 1
+            max_coords = branch_point_coordinates.max(axis=0) + 2
+            # Create a tuple of slices for each dimension
+            slices = tuple(
+                slice(min_coord, max_coord) for min_coord, max_coord in zip(min_coords, max_coords)
+                )
+
+            # Crop the image data using the slices
+            cropped_branch = branch[slices]
+            cropped_branch = morphology.binary_dilation(cropped_branch, footprint=np.ones((3, 3)))
+            cropped_data = self.data[slices]
+
+            touching_labels = np.logical_xor(cropped_branch, cropped_data == row['label']) * cropped_data
+            connecting_labels = np.unique(touching_labels)
+            connecting_labels = connecting_labels[connecting_labels != 0]
+
+            if len(connecting_labels) == 0:
+                self.features.iloc[i, self.features.columns.get_loc('object_type')] = 1
+                print('detected malformatted label: ', row['label'])
+                continue
+
+            self.graph.add_edge(connecting_labels[0], connecting_labels[1],
+                                    label=row['label'])
+
