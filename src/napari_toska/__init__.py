33ad4a68ea31232fb18ef7a879a3062058ea898a
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2025-03-16 15:55:40 +0100

    bump version

--- src/napari_toska/__init__.py
+++ src/napari_toska/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 from ._labeled_skeletonization import (generate_labeled_skeletonization,
                                        parse_single_skeleton,
                                        parse_all_skeletons,
